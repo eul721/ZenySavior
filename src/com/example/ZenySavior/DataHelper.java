23ac3236a856e22f48d34fc9dc338027ff50c5ee
Jacky <jacky@ymcnetwork.com>
2014-07-19 21:58:47 -0700

    Added "Add" button Completed basic input and adding functionalities

---
+++ src/com/example/ZenySavior/DataHelper.java
@@ -0,0 +1,7 @@
+package com.example.ZenySavior;
+
+/**
+ * Created by Jacky on 7/19/14.
+ */
+public class DataHelper {
+}
--- src/com/example/ZenySavior/HomeActivity.java
+++ src/com/example/ZenySavior/HomeActivity.java
@@ -4,21 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
+import java.util.*;
+
 public class HomeActivity extends Activity {
 
     //consts
     private final int N_DIGITS = 6;
-    private final String[] K_DIGITS_LABELS = {"thousands","hundreds", "tens", "ones","tenths","hundredths"};
+    private final double MAXIMUM_FACTOR = 1000;
 
-    private HashMap<String,NumberPicker> numberPickers = new HashMap<String,NumberPicker>(N_DIGITS);
+
+    private List<NumberPicker> numberPickers = new ArrayList<NumberPicker>();
 
 
     /**
@@ -26,33 +27,47 @@ public class HomeActivity extends Activity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //Create and set Layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-
-        //find a way to utilize iterations?
-        numberPickers.put("thousands",(NumberPicker)findViewById(R.id.thousands));
-        numberPickers.put("hundreds",(NumberPicker)findViewById(R.id.hundreds));
-        numberPickers.put("tens",(NumberPicker)findViewById(R.id.tens));
-        numberPickers.put("ones",(NumberPicker)findViewById(R.id.ones));
-        numberPickers.put("tenths",(NumberPicker)findViewById(R.id.tenths));
-        numberPickers.put("hundredths",(NumberPicker)findViewById(R.id.hundredths));
-
-        //Initializes month
-        ((TextView)findViewById(R.id.dateLabel)).setText(
-                new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance().getTime()));
-
-
-
-        initLimitsOfNumberPickers();
+        //Initialize Number Pickers
+        initNumberPickers();
         //[Add] Button
+        //The resource has been added to the xml
         ImageButton addButton = ((ImageButton)findViewById(R.id.addValueToDaily));
 
-        
+        //Initializes month
+        Date curTime = Calendar.getInstance().getTime();
+        String dateFormat = new SimpleDateFormat("yyyy/MM/dd").format(curTime);
+        ((TextView)findViewById(R.id.dateLabel)).setText(
+                dateFormat);
+
+        //text Labels
+        final TextView dailySpendingsLabel = (TextView)findViewById(R.id.dailySpendingsLabel);
+        final TextView monthlySpendingsLabel = (TextView)findViewById(R.id.monthlySpendingsLabel);
+
+        //Init the labels
+        //if (has_entry_for_today)
+        //set label as that
+        //else
+        dailySpendingsLabel.setText("Today's Spendings: $0.00");
+        monthlySpendingsLabel.setText("This Month's Spendings: $0.00");
+
+
+
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                testWidget(view);
+                /*
+                * If entry for today does not exist
+                *   Add entry to DB
+                * Else
+                *   Modify entry
+                * */
+                dailySpendingsLabel.setText("Today's Spendings: $" + BigDecimal.valueOf(getInputValue()).setScale(2,BigDecimal.ROUND_UP).toString());
+
             }
         });
 
@@ -61,8 +76,18 @@ public class HomeActivity extends Activity {
     }
 
     //default the min and max vals for all numberpickers
-    private void initLimitsOfNumberPickers(){
-        for (NumberPicker picker : this.numberPickers.values()){
+    private void initNumberPickers(){
+        // Future:
+        // Have program automatically create the NumPickers?
+        assert (numberPickers!=null);
+        numberPickers.add((NumberPicker)findViewById(R.id.thousands));
+        numberPickers.add((NumberPicker)findViewById(R.id.hundreds));
+        numberPickers.add((NumberPicker)findViewById(R.id.tens));
+        numberPickers.add((NumberPicker)findViewById(R.id.ones));
+        numberPickers.add((NumberPicker)findViewById(R.id.tenths));
+        numberPickers.add((NumberPicker)findViewById(R.id.hundredths));
+
+        for (NumberPicker picker : this.numberPickers){
             picker.setMinValue(0);
             picker.setMaxValue(9);
         }
@@ -70,6 +95,30 @@ public class HomeActivity extends Activity {
 
     private void testWidget(View widgetView){
         Log.d("Testing Widget","Currently testing " + widgetView.toString());
+    }
+
+    private void testWidget(View widgetView, String additionalMessage){
+        Log.d("Testing Widget", "Currently testing " + widgetView.toString() + ".\nCustom Message : "+additionalMessage);
+    }
+
+    private double getInputValue(){
+        /*
+        [0] = *1000
+        [1] = *100
+        [2] = *10
+        [3] = *1
+        [4] = *.1
+        [5] = *.01
+        * */
+        double curVal = 0.0;
+        double curValAtPicker;
+        for (int i=0;i<this.numberPickers.size();i++){
+            NumberPicker picker = this.numberPickers.get(i);
+            curValAtPicker = picker.getValue();
+            curValAtPicker = curValAtPicker * MAXIMUM_FACTOR / Math.pow(10.0,i);
+            curVal += curValAtPicker;
+        }
+        return curVal;
     }
 
 }
